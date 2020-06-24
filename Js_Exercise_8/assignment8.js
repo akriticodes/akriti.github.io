59060766b3bd2788ec57e2bc0e5191f32eff422c
Akriti <akritibagale@gmail.com>
2020-06-24 07:35:56 +0545

    Change

--- Js_Exercise_8/assignment8.js
+++ Js_Exercise_8/assignment8.js
@@ -6,21 +6,25 @@ var points =[
   { x: 100, y: 40 },
   { x: 120, y: 80 },
   { x: 140, y: 100 },
-  { x: 160, y: 120 }
+  { x: 200, y: 220 },
+  { x: 200, y: 190 },
+  { x: 250, y: 250 }
+
 ];
 
  var box = document.createElement('div');
- box.style.width='200px';
- box.style.height ='200px';
+ box.style.width='300px';
+ box.style.height ='300px';
  box.style.border = '5px solid #ddd';
  box.style.position ='relative';
+ box.style.margin = '0 auto';
  document.body.appendChild(box);
 
 
  for (var i = 0; i<points.length; i ++){
    var point = document.createElement('div');
-   point.style.height = '10px';
-   point.style.width = '10px';
+   point.style.height = '15px';
+   point.style.width = '15px';
    point.style.borderRadius ='50%';
    point.style.position ='absolute';
    point.style.left =(points[i]['x']) + 'px';
