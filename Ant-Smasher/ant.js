8e277d323352f59c86175099b0294f50babfdefc
Akriti <akritibagale@gmail.com>
2020-06-30 06:35:49 +0545

    Add File

---
+++ Ant-Smasher/ant.js
@@ -0,0 +1,127 @@
+class Ant{
+  constructor(container){
+      this.container = container;
+      this.diameter = 30 ;
+      this.x = Math.floor(Math.random() * (600 - this.diameter)) ;
+      this.y = Math.floor(Math.random() * (600 - this.diameter));
+      this.dy = Math.floor(Math.random() *1)+1;
+      this.dx = Math.floor(Math.random() *1)+1;
+      this.ant = document.createElement('div');
+  }
+
+  draw(){
+    this.ant.style.position = 'absolute';
+    this.ant.style.height = this.diameter + 'px';
+    this.ant.style.width = this.diameter + 'px';
+    //this.ant.style.backgroundImage = 'url(.im)';
+    this.ant.backgroundColor ="red";
+    this.ant.style.bottom = this.y + 'px';
+    this.ant.style.left = this.x + 'px';
+    this.ant.style.backgroundRepeat ='no-repeat';
+    this.container.appendChild(this.ant);
+  }
+
+  
+
+  
+
+  borderBarrier() {
+    if (this.x > (600 - this.diameter)){
+      this.dx = -Math.abs(this.dx);
+    }
+    if (this.x < 0){
+      this.dx = Math.abs(this.dx);
+    }
+    if (this.y > (600 - this.diameter)){
+      this.dy = -Math.abs(this.dy);
+    }
+    if (this.y < 0){
+      this.dy = Math.abs(this.dy);
+    }
+  }
+
+
+
+antGet(){
+  return this.ant;
+}
+
+newDirection(dxDir, dyDir){
+  this.dx = dxDir;
+  this.dy = dyDir;
+}
+
+newposition(){
+  this.x += 1;
+  this.y += 1;
+}
+
+direction(){
+  return [this.dx, this.dy];
+}
+
+
+position(){
+ return[this.radius, this.x, this.y];
+}
+
+proceed(){
+  this.x += this.dx;
+  this.y += this.dy;
+  this.ball.style.bottom = this.y + 'px';
+  this.ball.style.left = this.x + 'px';
+}
+
+
+ 
+var box = document.querySelector('.box');
+ 
+var antsArray = [];
+var antNumber = 5;
+
+function collisionHandler(){
+  for (i = 0; i < antNumber; i++)
+    for (j = i; j < antNumber; j++)
+      if (i !== j)
+        if(detectCollision(antsArray[i].getLocation(), antsArray[j].getLocation()))
+        {
+          antsArray[i].setVectors(-antsArray[i].getVectors()[0], -antsArray[i].getVectors()[1]);
+          antsArray[j].setVectors(-antsArray[j].getVectors()[0], -antsArray[j].getVectors()[1]);
+        }
+}
+
+function detectCollision(circle1, circle2){
+  let dx = circle1[1] - circle2[1];
+  let dy = circle1[2] - circle2[2];
+  let distance = Math.sqrt(dx * dx + dy * dy);
+
+  if (distance < (circle1[0]/2) + (circle2[0]/2)) {
+      return(true)
+  }
+}
+
+
+for(i= 0; i< antNumber; i++){
+  ant = new Ant(box,'#333');
+  ant.draw()
+  antsArray.push(ant);
+}
+
+function addListener(ant){
+  ant.antGet.addEventListener('click', function(){
+    ant.antGet.remove();
+    ants = ants.filter(item => item !== ant);
+    antNumber -= 1;
+  });
+}
+
+function antMove(){
+  for(i= 0; i<antNumber; i++){
+    antsArray[i].proceed();
+    antsArray[i].borderBarrier();
+  }
+  collisionHandler()
+  window.requestAnimationFrame(antMove);
+}
+
+antMove();
---
+++ Ant-Smasher/index.js
@@ -0,0 +1,6 @@
+var box = document.querySelector('.box');
+box.style.width = 600 +"px";
+box.style.height = 600 +"px";
+box.style.border = '5px solid black';
+box.style.position = "relative";
+document.body.appendChild(box);
