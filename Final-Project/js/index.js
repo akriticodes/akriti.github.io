473ca136c51b4d3047d53d3b46d8c5fabc4ee247
Akriti <akritibagale@gmail.com>
2020-07-07 23:47:44 +0545

    Add Transparant Layers

--- Final-Project/js/index.js
+++ Final-Project/js/index.js
@@ -5,31 +5,31 @@ var image = new Image;
 imageName ='star.jpg';
 image.src = './image/filters.jpg';
 
-ctx.drawImage(image, 0, 0, 400, 400);
+ctx.drawImage(image, 0, 0);
 
 
-function Layers(text){
-  var node = document.querySelector(".layers");   
-    var newDiv = document.createElement("div");
-    newDiv.classList.add("pic-one");
-      var eyeDiv = document.createElement("div");
-      eyeDiv.classList.add("eye-icon");
-          var aDiv = document.createElement("a");
-          aDiv.hrerf = "#";
-            var iDiv = document.createElement("i");
-            iDiv.classList.add("fa");
-            iDiv.classList.add("fa-eye");
-            aDiv.appendChild(iDiv);
+// function Layers(text){
+//   var node = document.querySelector(".layers");   
+//     var newDiv = document.createElement("div");
+//     newDiv.classList.add("pic-one");
+//       var eyeDiv = document.createElement("div");
+//       eyeDiv.classList.add("eye-icon");
+//           var aDiv = document.createElement("a");
+//           aDiv.href = "#";
+//             var iDiv = document.createElement("i");
+//             iDiv.classList.add("fa");
+//             iDiv.classList.add("fa-eye");
+//             aDiv.appendChild(iDiv);
 
-      eyeDiv.appendChild(aDiv);
-    newDiv.appendChild(eyeDiv);
-    var textDiv = document.createElement("div");
-    textDiv.classList.add("layer-title");
-    textDiv.innerHTML = text;
-    newDiv.appendChild(textDiv);
+//       eyeDiv.appendChild(aDiv);
+//     newDiv.appendChild(eyeDiv);
+//     var textDiv = document.createElement("div");
+//     textDiv.classList.add("layer-title");
+//     textDiv.innerHTML = text;
+//     newDiv.appendChild(textDiv);
  
-  node.appendChild(newDiv);  
+//   node.appendChild(newDiv);  
 
-}
+// }
 
-Layers(imageName);
+// Layers(imageName);
