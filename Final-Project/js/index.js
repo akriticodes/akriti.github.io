982d91338010bfff08cd8267cfcb134a87770138
Akriti <akritibagale@gmail.com>
2020-07-07 17:05:25 +0545

    Pop layer Name

---
+++ Final-Project/js/index.js
@@ -0,0 +1,35 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+var image = new Image;   
+imageName ='star.jpg';
+image.src = './image/filters.jpg';
+
+ctx.drawImage(image, 0, 0, 400, 400);
+
+
+function Layers(text){
+  var node = document.querySelector(".layers");   
+    var newDiv = document.createElement("div");
+    newDiv.classList.add("pic-one");
+      var eyeDiv = document.createElement("div");
+      eyeDiv.classList.add("eye-icon");
+          var aDiv = document.createElement("a");
+          aDiv.hrerf = "#";
+            var iDiv = document.createElement("i");
+            iDiv.classList.add("fa");
+            iDiv.classList.add("fa-eye");
+            aDiv.appendChild(iDiv);
+
+      eyeDiv.appendChild(aDiv);
+    newDiv.appendChild(eyeDiv);
+    var textDiv = document.createElement("div");
+    textDiv.classList.add("layer-title");
+    textDiv.innerHTML = text;
+    newDiv.appendChild(textDiv);
+ 
+  node.appendChild(newDiv);  
+
+}
+
+Layers(imageName);
---
+++ Final-Project/js/layoutfix.js
@@ -0,0 +1,3 @@
+function myFunction() {
+  document.querySelector(".tuning").style.display = "none";
+}
