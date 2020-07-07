8b05826e4cfb5046931fc6fcf7cf0498253f2de2
Akriti <akritibagale@gmail.com>
2020-07-07 17:54:36 +0545

    Slider Fix

--- Final-Project/js/layoutfix.js
+++ Final-Project/js/layoutfix.js
@@ -1,3 +1,15 @@
-function myFunction() {
-  document.querySelector(".tuning").style.display = "none";
-}
+sliderButton = document.getElementById('slider-button');
+sliderButton.addEventListener('click', function(){
+  let filterDiv = document.querySelector('.filters');
+  filterDiv.style.display = 'none';
+  let tuningDiv = document.querySelector('.tuning');
+  tuningDiv.style.display = 'block';
+})
+
+filterButton = document.getElementById('filter-button');
+filterButton.addEventListener('click', function(){
+  let filterDiv = document.querySelector('.filters');
+  filterDiv.style.display = 'block';
+  let tuningDiv = document.querySelector('.tuning');
+  tuningDiv.style.display = 'none';
+})
