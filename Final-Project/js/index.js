8e6d6c0173219528882208f67fd24f46f63be4dc
Akriti <akritibagale@gmail.com>
2020-07-09 01:07:58 +0545

    Layer Added

--- Final-Project/js/index.js
+++ Final-Project/js/index.js
@@ -2,29 +2,53 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 canvasDiv = document.querySelector('.column-two')
-canvasWidth = canvasDiv.clientWidth;
-canvasHeight = canvasDiv.clientHeight;
+let canvasWidth, canvasHeight;
+canvasWidth =800;
+canvasHeight = 590;
 canvas.height = canvasHeight;
 canvas.width = canvasWidth;
   
 var file = document.getElementById('getfile');
 
-  file.addEventListener("change",handleImage);
+file.addEventListener("change",handleImage);
 
+// Add-text-Code
+var addTextButton = document.getElementById('text-icon');
 
+addTextButton.addEventListener('click', function(){
+  layersArray[activeLayer].addText();
+  updateScreen();
+})
+
+ 
 function handleImage(e){
   var reader = new FileReader();
   reader.onload = function(event){
       var img = new Image();
       img.onload = function(){
-        // canvas.width = img.width;
-        // canvas.height = img.height;
-        // ctx.drawImage(img,0,0);
-          ctx.drawImage(img, 0, 0, canvasWidth, canvasHeight, 0,0, img.width, img.height);
+        image = new Layer('image',  layersArray.length);
+        image.AddImageToLayers(img);
+        layersArray.push(image);
+        makeActive(image);
+        addLayerEvent(image);  
+        updateScreen();
       }
       img.src = event.target.result;
   }
   reader.readAsDataURL(e.target.files[0]);     
+}
+
+
+
+// zoom-in/zoom-out
+
+document.onkeydown = function(e){
+  if (e.ctrlKey && e.keyCode == 38){
+    canvas.width += 10;
+    console.log(canvas.width)
+  }else if (e.ctrlKey && e.keyCode == 40){
+    canvas.width -= 10;
+  }
 }
 
 
---
+++ Final-Project/js/layers.js
@@ -0,0 +1,95 @@
+class Layer{
+  constructor(name, index ){
+    this.img;
+    this.text;
+    this.name = name;
+    this.index = index;
+    this.width = canvasWidth;
+    this.height = canvasHeight;
+    this.layerIndicatorDiv;
+    this.AddlayerstoList();
+    this.InitializeLayerCanvas();
+
+  }
+
+  AddlayerstoList(){
+    let node = document.querySelector(".layers");   
+    this.layerIndicatorDiv = document.createElement("div");
+    this.layerIndicatorDiv.classList.add("layerDiv");
+    let eyeDiv = document.createElement("div");
+    eyeDiv.classList.add("eye-icon");
+    let aDiv = document.createElement("a");
+    aDiv.href = "#";
+    let iDiv = document.createElement("i");
+    iDiv.classList.add("fa");
+    iDiv.classList.add("fa-eye");
+    aDiv.appendChild(iDiv);
+    eyeDiv.appendChild(aDiv);
+    this.layerIndicatorDiv.appendChild(eyeDiv);
+    this.textDiv = document.createElement("div");
+    this.textDiv.classList.add("layer-title");
+    this.textDiv.innerHTML = this.name;
+    this.layerIndicatorDiv.appendChild(this.textDiv);
+    node.appendChild(this.layerIndicatorDiv);  
+    
+  }
+
+  InitializeLayerCanvas(){//canvas=rectangle
+    ctx.beginPath();
+    ctx.fillStyle = "rgba(0,0,0,0.1)";
+    ctx.rect(0, 0, this.width, this.height);
+    ctx.fill();
+  } 
+  
+  AddImageToLayers(img){
+    this.img = img;
+  }
+
+  addText(){
+    this.text = 'Testing';
+    ctx.font = "30px Arial";
+  }
+
+  draw(){
+    ctx.fillStyle = '#ffffff'
+    if(this.img)
+      ctx.drawImage(this.img, 0, 0, canvasWidth, canvasHeight);
+    if(this.text)
+      ctx.fillText(this.text, 10, 50);
+  }
+}
+
+var activeLayer = 0;
+var layersArray = []
+
+function makeActive(layer){
+  layersArray[activeLayer].layerIndicatorDiv.classList.remove('activeLayerDiv');
+  activeLayer = layer.index;
+  layer.layerIndicatorDiv.classList.add('activeLayerDiv');
+}
+
+function addLayerEvent(layer){
+  layer.layerIndicatorDiv.addEventListener('click', function (){
+    makeActive(layer);
+  })
+}
+
+//Create New Layers
+layerButton = document.getElementById('layer-button');
+layerButton.addEventListener('click', function(){
+  layer = new Layer("Layer " + (layersArray.length + 1),  layersArray.length);
+  layersArray.push(layer);
+  makeActive(layer);
+  addLayerEvent(layer);
+})
+
+//update screen 
+function updateScreen(){
+  ctx.fillStyle = "#19141d";
+  ctx.rect(0, 0, canvasWidth, canvasHeight);
+  ctx.fill();
+  layersArray.forEach(function(layer){
+    layer.draw();
+  })
+}
+
