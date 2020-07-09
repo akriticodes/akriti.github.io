98ce59bec467122f1dae5bcf4f756c0e9e10c828
Akriti <akritibagale@gmail.com>
2020-07-09 09:25:58 +0545

    Add Delete Button

--- Final-Project/js/index.js
+++ Final-Project/js/index.js
@@ -1,26 +1,16 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-
 canvasDiv = document.querySelector('.column-two')
 let canvasWidth, canvasHeight;
 canvasWidth =800;
 canvasHeight = 590;
 canvas.height = canvasHeight;
 canvas.width = canvasWidth;
-  
+
+
+//add image to the canvas
 var file = document.getElementById('getfile');
-
 file.addEventListener("change",handleImage);
-
-// Add-text-Code
-var addTextButton = document.getElementById('text-icon');
-
-addTextButton.addEventListener('click', function(){
-  layersArray[activeLayer].addText();
-  updateScreen();
-})
-
- 
 function handleImage(e){
   var reader = new FileReader();
   reader.onload = function(event){
@@ -38,18 +28,23 @@ function handleImage(e){
   reader.readAsDataURL(e.target.files[0]);     
 }
 
+// Add-text-Code
+var addTextButton = document.getElementById('text-icon');
+addTextButton.addEventListener('click', function(){
+  layersArray[activeLayer].addText();
+  updateScreen();
+})
 
 
 // zoom-in/zoom-out
-
-document.onkeydown = function(e){
-  if (e.ctrlKey && e.keyCode == 38){
-    canvas.width += 10;
-    console.log(canvas.width)
-  }else if (e.ctrlKey && e.keyCode == 40){
-    canvas.width -= 10;
-  }
-}
+// document.onkeydown = function(e){
+//   if (e.ctrlKey && e.keyCode == 38){
+//     canvas.width += 10;
+//     console.log(canvas.width)
+//   }else if (e.ctrlKey && e.keyCode == 40){
+//     canvas.width -= 10;
+//   }
+// }
 
 
  
